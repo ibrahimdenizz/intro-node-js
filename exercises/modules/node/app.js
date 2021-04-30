328b659072bc037841de8f4570aa599daa82b714
ibrahim <deniz351961@gmail.com>
2021-05-01 00:34:47 +0300

    Convert Browser JavaScript Solution

---
+++ exercises/modules/node/app.js
@@ -0,0 +1,28 @@
+const App = require('./api');
+
+App.showPostsForCurrentUser = (userId, cb) => {
+  App.getPostsForUser(userId, (posts) => {
+    const postTemplates = posts.map((post) => {
+      return `
+      <div class="post">
+        ${post.title}
+        ${post.body}
+        ${post.createdBy}
+      </div>`;
+    });
+    cb(postTemplates);
+  });
+};
+
+App.showUserProfile = (userId, cb) => {
+  App.getUserById(userId, (user) => {
+    const profile = `
+      <div>
+        ${user.name}
+      </div>
+    `;
+    cb(user);
+  });
+};
+
+module.exports = App;
