18d91f05f42b1eba31d1c7592b2afa8e371c1e65
ibrahim <deniz351961@gmail.com>
2021-05-01 13:39:22 +0300

    Asynchronous Server Solution

--- exercises/api/server.js
+++ exercises/api/server.js
@@ -1,40 +1,55 @@
-const http = require('http')
-const url = require('url')
-const fs = require('fs')
-const path = require('path')
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
 
 /**
  * this function is blocking, fix that
  * @param {String} name full file name of asset in asset folder
  */
 const findAsset = (name) => {
-  const assetPath = path.join(__dirname, 'assets', name)
-  return fs.readFileSync(assetPath, {encoding: 'utf-8'}).toString()
-}
+  const assetPath = path.join(__dirname, 'assets', name);
+  return new Promise((resolve, reject) => {
+    fs.readFile(assetPath, { encoding: 'utf-8' }, (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
 
-const hostname = '127.0.0.1'
-const port = 3000
+const hostname = '127.0.0.1';
+const port = 3000;
 
 // log incoming request coming into the server. Helpful for debugging and tracking
-const logRequest = (method, route, status) => console.log(method, route, status)
+const logRequest = (method, route, status) =>
+  console.log(method, route, status);
 
-const server = http.createServer((req, res) => {
-  const method = req.method
-  const route = url.parse(req.url).pathname
+const server = http.createServer(async (req, res) => {
+  const method = req.method;
+  const route = url.parse(req.url).pathname;
   // this is sloppy, especially with more assets, create a "router"
   if (route === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(findAsset('index.html'))
-    logRequest(method, route, 200)
-    res.end()
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(await findAsset('index.html'));
+    logRequest(method, route, 200);
+    res.end();
+  } else if (route === '/style.css') {
+    res.writeHead(200, { 'Content-Type': 'text/css' });
+    res.write(await findAsset('style.css'));
+    logRequest(method, route, 200);
+    res.end();
   } else {
     // missing asset should not cause server crash
-    throw new Error('route not found')
-    res.end()
+    res.writeHead(404);
+    logRequest(method, route, 404);
+    res.end();
   }
   // most important part, send down the asset
-})
+});
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
